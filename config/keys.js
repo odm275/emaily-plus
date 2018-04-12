920bd9d07bf973ceac729b668cd5a15b12f71d72
odm275 <pomejia@gmail.com>
2018-04-12 18:56:43 -0500

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// keys.js; set up logic for prod or dev.
+
+if(process.env.NODE_ENV === 'production'){
+    //we are in production
+    module.exports = require('./prod');
+
+} else {
+    // we are in development
+    module.exports = require('./dev');
+}
