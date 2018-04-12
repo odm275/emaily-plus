ad04d0f76d5fcd0a9581e2e3abfc585e218edcc2
odm275 <pomejia@gmail.com>
2018-04-12 15:05:56 -0500

    Finished the basic oath flow

--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,42 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-passport.use(new GoogleStrategy(
-    {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    }, 
-    (acessToken, refreshToken, profile, done) => {
-        console.log('acess token ' + acessToken);
-        console.log('refresh token ' + refreshToken);
-        console.log('profile ' + profile);
-        }
-    )  
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    //Take user idenity and translate to cookie
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    //Take cookie and translate to user identity
+    User.findById(id)
+        .then(user => {
+            done(null,user);
+        });
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+        }, 
+        (acessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id })//find googleid, will or willnot exist
+                .then(existingUser => {
+                    if(existingUser){
+                        //  we already have a record with the given profile ID
+                        done(null,existingUser);
+                    } else {
+                        new User({ googleId: profile.id})
+                            .save()
+                            .then(user => done(null,user));
+                    }
+                });
+            }
+        )  
 );
