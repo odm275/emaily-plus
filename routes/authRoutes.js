2f855c3d9a239c79cad945767aba12081ffb7fd7
odm275 <pomejia@gmail.com>
2018-04-13 21:41:17 -0500

    front end for login added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,11 @@ module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']  
     }));
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/google/callback', passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys');
+        }
+    );
     app.get('/api/logout', (req,res) => {
         req.logout();
         res.send(req.user); //Shouldn't be able to return anything
--- services/passport.js
+++ services/passport.js
@@ -26,18 +26,16 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         }, 
-        (acessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id })//find googleid, will or willnot exist
-                .then(existingUser => {
-                    if(existingUser){
+        async (acessToken, refreshToken, profile, done) => {
+           const existingUser =  await User.findOne({ googleId: profile.id })//find googleid, will or willnot exist
+                    if (existingUser){
                         //  we already have a record with the given profile ID
-                        done(null,existingUser);
-                    } else {
-                        new User({ googleId: profile.id})
-                            .save()
-                            .then(user => done(null,user));
-                    }
-                });
+                       return done(null,existingUser);
+                    } 
+                    const user =  await new User({ googleId: profile.id}).save();
+                    done(null,user);
             }
         )  
 );
+
+
