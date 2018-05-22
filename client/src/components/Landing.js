ab305d2f9f662b5d21a1b731875564a815d89e26
odm275 <pomejia@gmail.com>
2018-05-22 10:28:13 -0500

    added test credential instructions

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Landing = () => {
-    return(
-        <div style={{textAlign: 'center'}}>
-            <h1>
-                Emaily!
-            </h1>
-            Collect feedback from your users
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Emaily!</h1>
+      <h4> Collect feedback from your users</h4>
+
+      <h4>If you are a test user, login with the following.</h4>
+      <h5>
+        Login with test account:<br /> User: testd667@gmail.com <br />Pass:
+        nkDOoEX6xElSCD2E!
+      </h5>
+      <h4>If you wanna add credits, use the following test credentials</h4>
+      <h5>
+        Card Number: 4242 4242 4242 4242 <br />For expiration enter a random
+        date that’s not expired by the current date. <br />For the security
+        number enter a 3-4 digit random number.
+      </h5>
+    </div>
+  );
 };
 
 export default Landing;
