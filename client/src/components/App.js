be1602a0d4d9e431b699fba3de43506b8aaaaf1c
odm275 <pomejia@gmail.com>
2018-05-25 08:09:33 -0500

    added refference link

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import * as  actions from '../actions';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-import Header from './Header';
-import Landing from './Landing';
-import Dashboard from './Dashboard';
-import SurveyNew from './surveys/SurveyNew';
+import Header from "./Header";
+import Footer from "./Footer";
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
-   componentDidMount() {
-        this.props.fetchUser(); //action dispatcher
-   }
-    render(){
-        return(
-        <div className="container">
-            <BrowserRouter>
-            <div>
-                <Header/>
-                <Route exact path="/" component={Landing}/>
-                <Route exact path="/surveys" component={Dashboard}/>
-                <Route path="/surveys/new" component={SurveyNew}/>
-            </div>
-            </BrowserRouter>
-        </div>  
-        );
-    }
+  componentDidMount() {
+    this.props.fetchUser(); //action dispatcher
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default connect(null, actions)(App);
---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="footer">
+    <p style={{ fontSize: "1.5rem" }}>
+      {" "}
+      <a href="https://odm275.github.io/portfolio/">Coded by Oscar Mejia</a>
+    </p>
+  </footer>
+);
+
+export default Footer;
